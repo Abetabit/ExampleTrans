b12e53df842318f3fb504fcaed0042e3eb8f059e
Anne-Marie Boerlage <a.boerlage@betabit.nl>
2019-10-25 09:49:14 +0200

    infrastructure project

---
+++ Transavia.Infrastructure/ContextDB/Seed.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using Transavia.Core.Entities;
+using Transavia.Core.Enums;
+
+namespace Transavia.Infrastructure.ContextDB
+{
+    public static class Seed
+    {
+        public async static void SeedData(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            SeedRoles(roleManager);
+            SeedUsers(userManager);
+        }
+
+        public static void SeedRoles(RoleManager<IdentityRole> roleManager)
+        {
+
+            if (!roleManager.RoleExistsAsync("Administrator").Result)
+            {
+                IdentityRole role = new IdentityRole
+                {
+                    Name = "Administrator",
+                    NormalizedName = "Administrator".ToUpper()
+                };
+
+                IdentityResult roleResult = roleManager.CreateAsync(role).Result;
+            }
+
+            if (!roleManager.RoleExistsAsync("User").Result)
+            {
+                IdentityRole role = new IdentityRole
+                {
+                    Name = "User",
+                    NormalizedName = "User".ToUpper()
+                };
+
+                IdentityResult roleResult = roleManager.CreateAsync(role).Result;
+            }
+        }
+
+        public async static void SeedUsers(UserManager<User> userManager)
+        {
+
+            if (userManager.FindByEmailAsync("admin@betabit.nl").Result == null)
+            {
+                User user = new User
+                {
+                    Email = "admin@betabit.nl",
+                    EmailConfirmed = true,
+                    FirstName = "Bas",
+                    LastName = "van der Meer",
+                    PhoneNumber = "0612345678",
+                    UserName = "admin@betabit.nl",
+                    Gender = Gender.Male,
+                    DateOfBirth = new DateTime(1991 - 05 - 03),
+                };
+                string password = "12345678";
+                IdentityResult userResult = userManager.CreateAsync(user, password).Result;
+                IdentityResult roleResult = userManager.AddToRoleAsync(user, "Administrator").Result;
+            }
+            if (userManager.FindByEmailAsync("user@betabit.nl").Result == null)
+            {
+                User user = new User
+                {
+                    Email = "user@betabit.nl",
+                    EmailConfirmed = true,
+                    FirstName = "Paula",
+                    LastName = "Straat",
+                    PhoneNumber = "0612345678",
+                    UserName = "user@betabit.nl",
+                    Gender = Gender.Female,
+                    DateOfBirth = new DateTime(1991 - 05 - 03)
+                };
+
+                string password = "12345678";
+                IdentityResult userResult = userManager.CreateAsync(user, password).Result;
+                IdentityResult roleResult = userManager.AddToRoleAsync(user, "User").Result;
+            }
+
+            if (userManager.FindByEmailAsync("user2@betabit.nl").Result == null)
+            {
+                User user = new User
+                {
+                    Email = "user2@betabit.nl",
+                    EmailConfirmed = true,
+                    FirstName = "Luke",
+                    LastName = "de Leeuw",
+                    PhoneNumber = "0612345678",
+                    UserName = "user2@betabit.nl",
+                    Gender = Gender.Female,
+                    DateOfBirth = new DateTime(1991 - 05 - 03)
+                };
+
+                string password = "12345678";
+                IdentityResult userResult = userManager.CreateAsync(user, password).Result;
+                IdentityResult roleResult = userManager.AddToRoleAsync(user, "User").Result;
+            }
+        }
+    }
+}
---
+++ Transavia.Infrastructure/EnumHelpers/EnumExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Transavia.Core.EnumHelpers;
+
+namespace Transavia.Infrastructure.EnumHelpers
+{
+    public static class EnumExtensions
+    {
+        public static List<EnumValue> GetValues<T>()
+        {
+            List<EnumValue> values = new List<EnumValue>();
+            foreach (var itemType in Enum.GetValues(typeof(T)))
+            {
+                //For each value of this enumeration, add a new EnumValue instance
+                values.Add(new EnumValue()
+                {
+                    Description = Enum.GetName(typeof(T), itemType),
+                    Id = (int)itemType
+                });
+            }
+            return values;
+        }
+    }
+}
