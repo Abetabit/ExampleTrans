372cbdc56e61ab6272628d51581e4f9ddeeb2d74
Anne-Marie Boerlage <a.boerlage@betabit.nl>
2019-10-25 09:45:39 +0200

    core project

---
+++ Transavia.Core/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Transavia.Core.Entities;
+
+
+namespace Transavia.Core.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T GetById(int id);
+        void Add(T entity);
+        void Delete(T entity);
+        IEnumerable<T> GetAll();
+
+        Task<IList<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<bool> SaveChangesAsync();
+        IEnumerable<T> GetAll(ISpecification<T> spec);
+    }
+}
---
+++ Transavia.Core/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Transavia.Core.Entities;
+
+namespace Transavia.Core.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IList<User>> GetAllAsync();
+        Task<User> GetByIdAsync(string id);
+    }
+}
